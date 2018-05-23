f1def1af96a0438f1106c401cba7b80a620ff97d
Josh Asplund <joshasplund@gmail.com>
2018-05-23 15:38:50 -0500

    Adds node version as a build arg

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+IMAGE_NAME="mythril"
+VERSION_NUMBER="0.0.1"
+
 if [ -z "$1" ];
 then
-  docker build -t mythril:0.0.1 src/docker/.
+  docker build -t "$IMAGE_NAME:$VERSION_NUMBER" src/docker/.
 else
-  docker build -t mythril:0.0.1 src/docker/. "$1"
+  docker build -t "$IMAGE_NAME:$VERSION_NUMBER" src/docker/. "$1"
 fi
