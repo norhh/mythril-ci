1bedbb0a427ee43107e927312ef724585d2d8737
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-09-22 01:39:39 +0200

    Adds caching of blockchain blocks between CircleCI builds

--- dev_test_environment_release.sh
+++ dev_test_environment_release.sh
@@ -5,8 +5,18 @@ NAME=mythril/dev_test_environment
 VERSION_TAG=${NAME}:${CIRCLE_TAG#?}
 LATEST_TAG=${NAME}:latest
 
+if [ ! -e ./src/docker/.cache ];
+  then mkdir ./src/docker/.cache;
+fi
+
 docker build -t ${VERSION_TAG} src/docker/.
 docker tag ${VERSION_TAG} ${LATEST_TAG}
+
+if [ ! -e ./src/docker/.cache/.ethereum ];
+then \
+  docker create --name app $VERSION_TAG; \
+  docker cp app:/root/.ethereum ./src/docker/.cache; \
+fi
 
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 
